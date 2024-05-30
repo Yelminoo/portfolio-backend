23efc3b40cef6f7a9e149d9f2dff5d3c47b07da7
Yelminoo <yelminoo2015@gmail.com>
2024-05-30 22:54:56 +0700

    fix updates

--- api/index.js
+++ api/index.js
@@ -168,13 +168,14 @@ app.get("/api", (req, res) => {
   });
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   console.log("process.env" + process.env.NODE_ENV);
+//   app.use(express.static(path.join(__dirname, "../client/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+//   });
+// }
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
